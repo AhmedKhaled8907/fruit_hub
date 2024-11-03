02d59b8fd4eb0508b4de827ea663ccdad314d93d
Ahmed Khaled <ahmedkh8907@gmail.com>
2024-11-03 04:41:52 +0200

    create custom app bar done

---
+++ lib/core/utils/widgets/custom_app_bar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:fruit_hub/core/utils/app_text_styles.dart';
+
+AppBar customAppBar(context, {required String title}) {
+  return AppBar(
+    backgroundColor: Colors.white,
+    elevation: 0,
+    title: Text(
+      title,
+      style: TextStyles.bold19,
+    ),
+    centerTitle: true,
+    leading: IconButton(
+      icon: const Icon(Icons.arrow_back_ios),
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+    ),
+  );
+}
--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/utils/widgets/custom_app_bar.dart';
+import 'widgets/login_view_body.dart';
+
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
   static const routeName = '/login';
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: customAppBar(
+        context,
+        title: 'تسجيل الدخول',
+      ),
+      body: const LoginViewBody(),
+    );
   }
 }
